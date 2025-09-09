3de663d1a46d4744b6530399c9c6ff98c707b4d4
Erick Muke <muke7881@gmail.com>
2025-09-09 03:08:07 -0600

    fix(date): Fixed date format to split timestamp

--- src/controllers/comments.controllers.ts
+++ src/controllers/comments.controllers.ts
@@ -1,37 +1,54 @@
-import { db } from "../lib/db.js";
+import { db } from '../lib/db.js';
 import {
-	comments,
-	type InsertComment,
-	type SelectComment,
-} from "../schemas/comments.js";
+  comments,
+  type InsertComment,
+  type SelectComment,
+} from '../schemas/comments.js';
 
 export async function GetAllComments(): Promise<SelectComment[]> {
-	const allComments = await db.select().from(comments).all();
-	return allComments;
+  const allComments = (
+    await db
+      .select({
+        comment_id: comments.comment_id,
+        name: comments.name,
+        job: comments.job,
+        description: comments.description,
+        direction: comments.direction,
+        country_flag: comments.country_flag,
+        created_at: comments.created_at,
+      })
+      .from(comments)
+      .all()
+  ).map((comment) => ({
+    ...comment,
+    created_at: new Date(comment.created_at).toISOString().slice(0, 10),
+  }));
+
+  return allComments;
 }
 
 export async function PostComments({
-	name,
-	job,
-	description,
-	country_flag,
-}: Omit<InsertComment, "direction">): Promise<SelectComment> {
-	// Contar los comentarios actuales para alternar la dirección
-	const existing = await db.select().from(comments);
-	const direction = existing.length % 2 === 0 ? "left" : "bottom";
+  name,
+  job,
+  description,
+  country_flag,
+}: Omit<InsertComment, 'direction'>): Promise<SelectComment> {
+  // Contar los comentarios actuales para alternar la dirección
+  const existing = await db.select().from(comments);
+  const direction = existing.length % 2 === 0 ? 'left' : 'bottom';
 
-	// Insertar el nuevo comentario
-	const result = await db
-		.insert(comments)
-		.values({
-			name,
-			job: job ?? null,
-			description,
-			direction,
-			country_flag,
-			created_at: new Date().toISOString(),
-		})
-		.returning();
+  // Insertar el nuevo comentario
+  const result = await db
+    .insert(comments)
+    .values({
+      name,
+      job: job ?? null,
+      description,
+      direction,
+      country_flag,
+      created_at: new Date().toISOString(),
+    })
+    .returning();
 
-	return result[0];
+  return result[0];
 }
